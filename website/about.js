8abbc93c3a56e860b5ff0c42e82644cdf8fd0078
Saral Sainju <sainju.saral433@gmail.com>
2023-01-19 22:23:49 +0545

    render using js and json

---
+++ website/about.js
@@ -0,0 +1,20 @@
+import data from "../data/data.json" assert { type: "json" };
+
+let container = document.getElementById("container");
+let htmlElements = "";
+
+for (let i = 0; i < 2; i++) {
+  htmlElements += `<div class="col-4">
+  <img src=${data.img[i]}>
+  <h4>${data.name[i]}</h4>
+  <div class="rating">
+      <i class="fa fa-star"></i>
+      <i class="fa fa-star"></i>
+      <i class="fa fa-star"></i>
+      <i class="fa fa-star"></i>
+      <i class="fa fa-star"></i>
+  </div>
+  <p>${data.price[i]}</p>
+</div>`;
+}
+container.innerHTML = htmlElements;
